95429ed32fa9bb43ac0577057d30c61f6d33c6db
Mustafa <umpp101@gmail.com>
2020-06-13 11:37:25 -0700

    login & signup components

---
+++ frontend/src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class home extends Component {
+    render() {
+        return (
+            <div>
+                <h1> i'm suppose to be the HOMEPAGE!</h1>
+            </div>
+        )
+    }
+}
+
+export default home
