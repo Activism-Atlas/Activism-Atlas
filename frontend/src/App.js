996140d23429ac861c14349998f7e491df8a7b3c
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-14 12:47:00 -0600

    Add error handling for supporter fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import styles from './app.module.css';
 import Login from "./components/login/login";
 import Home from "./components/home/home";
 import Navbar from "./components/navbar/navbar";
-import NewSupporter from "./components/newsupporter/newsupporter"
 
 class App extends Component {
   constructor() {
@@ -16,29 +15,30 @@ class App extends Component {
     };
   };
 
-
-
   handleLoginSubmit = async (event, loginInfo) => {
     event.preventDefault();
-    const fetchUrl = "http://localhost:8000/auth/token";
-    const settings = {
-      method: "POST", headers: { "Content-Type": "application/json", Accept: "application/json" },
-      body: JSON.stringify({ username: loginInfo.username, password: loginInfo.password })
-    };
-    const response = await fetch(fetchUrl, settings);
-    const postData = await response.json();
-    console.log(postData)
-    if (!!postData.error === true) return null // we will need to write some logic incase an error comes from the login attempt.
-    localStorage.setItem("token", postData.token);
-    await this.setState({ loggedIn: true })
-    this.props.history.push("/home"); // go to route but doesn't render the component when you login
+    try {
+      const fetchUrl = "http://localhost:8000/auth/token";
+      const settings = {
+        method: "POST", headers: { "Content-Type": "application/json", Accept: "application/json" },
+        body: JSON.stringify({username: loginInfo.username,password: loginInfo.password})
+      };
+      const response = await fetch(fetchUrl, settings);
+      if (!response.ok) {
+        throw new Error('Invalid Login');
+      }
+      const postData = await response.json();
+      localStorage.setItem("token", postData.token);
+      this.setState({loggedIn: true});
+    }
+    catch(e) {
+      console.log(e.message);
+    }
   };
 
   handleLogout = async () => {
-    console.log("clicked")
     localStorage.removeItem("token");
     await this.setState({ loggedIn: false })
-    this.props.history.push("/login");
   }
 
   fetchSupporters = async () => {
@@ -52,37 +52,26 @@ class App extends Component {
     this.setState({
       supporters: apiData
     })
-
   }
 
-
   render() {
-    console.log(this.state)
-    console.log(this.props)
     return (
       <Router>
         <div className="App">
           {/* Top NavBar Starts here, with the "Activism Atlas" routing to homepage and buttons switch between login and logout*/}
           <Navbar loggedIn={this.state.loggedIn} handleLogout={this.handleLogout} />
-          {/* NavBar Ends */}
-
-          {/* Rest of this is the routes: "/", "/login", "/signup" */}
           <div className={styles.mainBodyDiv}>
             <Switch>
               <Route exact path="/" render={(props) => (
                 <Login {...props} handleLogin={this.handleLoginSubmit} />)} /> {/* handleLogin passed down to the login, to get the inputed info*/}
               <Route exact path="/login" render={(props) => (
                 <Login {...props} handleLogin={this.handleLoginSubmit} />)} />
-              <Route exact path="/newsupporter" render={(props) => (
-                <NewSupporter {...props} handleSignup={this.handleSignupSubmit} />)} />  {/* handleSignUp passed down to the login, to get the inputed info*/}
-              {(this.state.loggedIn) ? (
-                <Route exact path="/home" render={(props) => (
-                  <Home {...props} supporters={this.state.supporters} />)} />
-              ) : (
-                  <Redirect to="/login" />
-                )}
+              <Route exact path="/signup" render={(props) => (
+                <SignUp {...props} handleSignup={this.handleSignupSubmit} />)} />  {/* handleSignUp passed down to the login, to get the inputed info*/}
+              <Route exact path="/home" render={(props) => (
+                <Home {...props} />)} />
             </Switch>
-
+            {(!this.state.loggedIn) ? <Redirect to="/login" /> : <Redirect to="/home" />}
           </div>
         </div>
       </Router>
