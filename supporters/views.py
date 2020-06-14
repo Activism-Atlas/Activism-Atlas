fa9a1dcab38b4cbf5d941475e7b8fde11b8a3361
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-13 21:54:37 -0600

    Create SupporterView

--- supporters/views.py
+++ supporters/views.py
@@ -8,8 +8,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, mixins, permissions
 from rest_framework.response import Response
 
-from .models import District
-from .serializers import DistrictSerializer
+from .models import District, Supporter
+from .serializers import DistrictSerializer, SupporterSerializer
 
 # *****************************************************************************
 # DistrictViewSet
@@ -29,3 +29,21 @@ class DistrictViewSet(mixins.CreateModelMixin,
     permission_classes = [permissions.IsAuthenticated]
     queryset = District.objects.all()
     serializer_class = DistrictSerializer
+
+# *****************************************************************************
+# SupporterViewSet
+# *****************************************************************************
+
+class SupporterViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.RetrieveModelMixin,
+                      viewsets.GenericViewSet):
+
+    """
+    A ViewSet to CREATE, LIST, UPDATE, and RETRIEVE districts
+
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = SupporterSerializer
+    queryset = Supporter.objects.all()
