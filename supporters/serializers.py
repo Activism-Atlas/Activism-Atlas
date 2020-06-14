3147b7ac8f0f09b01a6803c34501d15c1cd19e8b
Leslie Park <yspark90@gmail.com>
2020-06-13 23:54:25 -0700

    Add update method for SupporterSerializer.

--- supporters/serializers.py
+++ supporters/serializers.py
@@ -109,9 +109,9 @@ class SupporterSerializer(serializers.ModelSerializer):
     address = AddressSerializer(required=False)
     causes = CauseSerializer(required=False, many=True)
 
-    def _get_and_create_district(self, data):
+    def _get_or_create_district(self, data):
         """
-        Helper method to create District object from
+        Helper method to get or create District object from
         address validated data
 
         Args:
@@ -127,7 +127,12 @@ class SupporterSerializer(serializers.ModelSerializer):
         if not data:
             return
         
-        district, _ = District.objects.get_or_create(**data)
+        district_id = data.pop('id', None)
+        if not district_id:
+            district = District.objects.create(**data)
+        else:
+            district = District.objects.get(id=district_id)
+
         return district
 
     def create(self, validated_data):
@@ -148,7 +153,7 @@ class SupporterSerializer(serializers.ModelSerializer):
             district = self._get_and_create_district(district_data)
             address_data['district'] = district
 
-            address = Address.objects.create(**address_data)
+            address, _ = Address.objects.get_or_create(**address_data)
             supporter.address = address
             supporter.save(update_fields=['address'])
         
@@ -157,6 +162,66 @@ class SupporterSerializer(serializers.ModelSerializer):
             supporter.causes.add(**cause)
         
         return supporter
+    
+    # TODO: There's probably a better way to do this
+    def update(self, instance, validated_data):
+        """
+        Override to update address and causes
+
+        """
+
+        address_data = validated_data.pop('address', None)
+        causes_data = validated_data.pop('causes', None)
+        super().update(instance, validated_data)
+
+        if address_data is None and causes_data is None:
+            return instance
+        
+        if address_data is not None:
+            district_data = address_data.pop('district', None)
+            district = self._get_or_create_district(district_data)
+            address_data['district'] = district
+
+            address_id = address_data.pop('id', None)
+            if address_id:
+                address = Address.objects.get(id=address_id)
+                for key, val in address_data.items():
+                    setattr(address, key, val)
+                address.save()
+            else:
+                address = Address.objects.create(**address_data)
+            
+            instance.address = address
+            instance.save(update_fields=['address'])
+        
+        if causes_data is None:
+            return instance
+
+        # TODO: Need to change relationship between causes and supporters
+        existing_causes = instance.causes.all()
+        existing_cause_ids = instance.causes.values_list('id', flat=True)
+
+        created_or_updated_cause_ids = set()
+        for cause in causes_data:
+            cause_id = cause.pop('id', None)
+            if (not cause_id) or (cause_id in existing_cause_ids):
+                cause_obj, created = instance.causes.update_or_create(
+                    pk=cause_id, defaults={**cause}
+                )
+            else:
+                Cause.objects.filter(id=cause_id).update(**cause)
+                cause_obj = Cause.objects.get(id=cause_id)
+                instance.causes.add(cause_obj)
+
+            created_or_updated_cause_ids.add(cause_obj.id)
+        
+        to_remove_causes_qs = existing_causes.exclude(
+            id__in=created_or_updated_cause_ids
+        )
+        for cause in to_remove_causes_qs:
+            instance.causes.remove(cause)
+        
+        return instance
 
     class Meta:
         model = Supporter
